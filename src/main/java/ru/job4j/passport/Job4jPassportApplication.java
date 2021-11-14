e2d6e2cca234f12d6b69aa6ebc9faede07e9d1cb
Ilya Pavlov <ilya.pavlov@list.ru>
2021-11-14 15:15:52 +0300

    1. Синхронный обмен сообщений [#458496] Работает отправка объекта PassportDto в kafka

--- src/main/java/ru/job4j/passport/Job4jPassportApplication.java
+++ src/main/java/ru/job4j/passport/Job4jPassportApplication.java
@@ -2,8 +2,10 @@ package ru.job4j.passport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Job4jPassportApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/ru/job4j/passport/kafka/PassportKafkaService.java
@@ -0,0 +1,42 @@
+package ru.job4j.passport.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import ru.job4j.passport.kafka.dto.PassportDto;
+
+@Service
+public class PassportKafkaService {
+
+    @Autowired
+    private KafkaTemplate<String, PassportDto> greetingKafkaTemplate;
+
+    @Value(value = "${greeting.topic.name}")
+    private String greetingTopicName;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Scheduled(fixedRateString = "10000")
+    public void sendMessage() {
+
+        PassportDto passportDto1 = new PassportDto("Ilya","ilya@mail.ru");
+//        PassportDto passportDto2 = new PassportDto("Petr","petr@mail.ru");
+//        List<PassportDto> passportDtos = Arrays.asList(passportDto1, passportDto2);
+
+        logger.info("send message = {}", passportDto1);
+        Message<PassportDto> message = MessageBuilder
+                .withPayload(passportDto1)
+                .setHeader(KafkaHeaders.TOPIC, greetingTopicName)
+                .build();
+        greetingKafkaTemplate.send(greetingTopicName, passportDto1);
+
+    }
+
+}
